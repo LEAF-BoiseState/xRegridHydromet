29ce2ab33bf6cd84ba5cd03705db2541a9be95e6
Lejo <lejoflores@boisestate.edu>
2022-01-16 16:38:57 -0700

    debugged regridding file

--- src/RegridData.py
+++ src/RegridData.py
@@ -9,30 +9,16 @@ import datetime
 
 def RegridData(config_data,regridder=None):
 
-    # This is a stub... data regridding will be added here
-    start = time.time()
+    start = time.time() # Start timing
 
-    # If regridder is none, the user wants to reuse a previous weight file
-    if(regridder is None):
-        
-        # If regridder is None, check to make sure 'regridder_fname' is in the config file
-        if('regridder_fname' not in config_data['jobinfo']):
-            # If not raise exception
-            raise Exception('If build_weights is False, then regridder file name must be specified') 
-
-        # If passing, get the regridder file name
-        regridder_fname = config_data['jobinfo']['regridder_fname']
-
-    # Get the metadata for the source and destination grids
-    src_gridinfo = getGridMetaData.getGridMetaData(config_data['srcinfo']['src_gridinfo'])
-
-    # Get the source directory where the grid data is stored
-    dest_gridinfo = getGridMetaData.getGridMetaData(config_data['destinfo']['dest_gridinfo'])
+    #=========================================#
+    # Retrieve information about source files # 
+    #=========================================#
 
     # We are doing file-by-file regridding. Why, when we could open as an xArray dataset?
     # Because we can't be sure on the correct dimension for contatenation.
     if('src_dir' not in config_data['srcinfo']):
-        raise Exception()
+        raise Exception('Location of source grids not provided in config file')
 
     # Get the source folder
     src_dir = config_data['srcinfo']['src_dir']
@@ -44,7 +30,14 @@ def RegridData(config_data,regridder=None):
             'contain a string file name pattern with which to search for files to regrid')
 
     src_file_pattern = config_data['srcinfo']['srcfile_pattern'] # Get the search string pattern
-    src_files = glob.glob(src_file_pattern) # Get a list of source grid files to regrid
+    src_files = glob.glob(src_dir+src_file_pattern+'*') # Get a list of source grid files to regrid
+
+    # Get the metadata for the source and destination grids
+    src_gridinfo = getGridMetaData.getGridMetaData(config_data['srcinfo']['src_gridinfo'])
+
+    #=============================================#
+    # Retrieve information about destination grid # 
+    #=============================================#
 
     # Get the destination folder of the destination regridded files
     # Error trap to ensure the user provided a folder in which to save regridded files
@@ -62,18 +55,37 @@ def RegridData(config_data,regridder=None):
     # Get the file name pattern for destination files
     dest_file_pattern = config_data['destinfo']['destfile_pattern']
 
-    # Read regridder if we are reusing weights
+    # Get the source directory where the grid data is stored
+    dest_gridinfo = getGridMetaData.getGridMetaData(config_data['destinfo']['dest_gridinfo'])
+
+    #======================================================#
+    # Prepare the regridder, if it was not passed as input # 
+    #======================================================#
+    # If the regridder is not passed as input, assuming the user wants 
+    # passed a filename to a previously created weight file. Open it now
     if(regridder is None):
+        
+        # If regridder is None, check to make sure 'regridder_fname' is in the config file
+        if('regridder_fname' not in config_data['jobinfo']):
+            # If not raise exception
+            raise Exception('If build_weights is False, then regridder file name must be specified') 
+
         # Prepare the source grid for input to xESMF regridder
-        src_grid = getGridDictForESMF.getGridDictForESMF(src_gridinfo)
+        src_grid_dict = getGridDictForESMF.getGridDictForESMF(src_gridinfo)
 
         # Prepare the destination grid for input to xESMF regridder
-        dest_grid = getGridDictForESMF.getGridDictForESMF(dest_gridinfo)
+        dest_grid_dict = getGridDictForESMF.getGridDictForESMF(dest_gridinfo)
 
-        regridder = xe.Regridder(src_grid, dest_grid, config_data['jobinfo']['xesmf_method'], 
+        # If passing, get the regridder file name
+        regridder_fname = config_data['jobinfo']['regridder_fname']
+
+        regridder = xe.Regridder(src_grid_dict, dest_grid_dict, config_data['jobinfo']['xesmf_method'], 
             weights=regridder_fname)
 
-    # Loop through files
+    #===============================================#
+    # Loop through the source grid files and regrid # 
+    #===============================================#
+
     for src_file in src_files:        
 
         # Open the file
@@ -82,11 +94,26 @@ def RegridData(config_data,regridder=None):
         # Apply the regridder
         ds_out = regridder(ds_src)
 
-        # Save the output
-        out_fname = dest_dir + src_file.replace(src_file_pattern, dest_file_pattern)
-        ds_out.to_netcdf(out_fname)
+        # Check and set the correct coordiates 
+        if(dest_gridinfo['latvar_name'] != 'lat'):
+            ds_out = ds_out.rename({'lat': dest_gridinfo['latvar_name']})
 
-    end = time.time()
+        if(dest_gridinfo['lonvar_name'] != 'lon'):
+            ds_out = ds_out.rename({'lon': dest_gridinfo['lonvar_name']})
+
+        # Check and set dimension names
+        if('xdimname' in dest_gridinfo):
+            ds_out = ds_out.rename({'x': dest_gridinfo['xdimname']})
+
+        if('ydimname' in dest_gridinfo):
+            ds_out = ds_out.rename({'y': dest_gridinfo['ydimname']})
+
+        # Prepare the filename and save the output
+        out_fname = src_file.split('/')[-1] # Get just the file name, without the path
+        out_fname = out_fname.replace(src_file_pattern, dest_file_pattern) # Replace the search pattern with destination
+        ds_out.to_netcdf(dest_dir + out_fname) # Save to NetCDF file
+
+    end = time.time() # Stop timing
 
     dt = datetime.timedelta(seconds=(end-start))
     print('Completed data regridding process in elapsed time of '+str(dt))
